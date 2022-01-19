e94fc841777418b06d860be6aa527c67808b0bec
Mingo <windfeeling8@gmail.com>
2022-01-19 17:01:18 +0800

    Optimize log output writer code

--- Components/Log/LogOutputWriter.php
+++ Components/Log/LogOutputWriter.php
@@ -5,18 +5,6 @@ class LogOutputWriter extends LogWriter
 {
     public function write($logMsg)
     {
-        $dstFile = 'php://output';
-        if (isPhpCommandMode()) {
-            $dstFile = 'php://stdout';
-        }
-        $fileDesc = fopen($dstFile, 'w');
-        if ($fileDesc) {
-            $writeLen = fwrite($fileDesc, $logMsg);
-            fclose($fileDesc);
-
-            return $writeLen;
-        } else {
-            return false;
-        }
+        return file_put_contents('php://output', $logMsg, FILE_APPEND);
     }
 }
