03e9f8475b39500c74586a866dd488d622f29266
Mingo <windfeeling8@gmail.com>
2022-05-17 10:39:26 +0800

    feat: add redis lock owner parameter

--- Components/Redis/RedisLockProvider.php
+++ Components/Redis/RedisLockProvider.php
@@ -25,9 +25,10 @@ class RedisLockProvider extends Provider
      * @param string $name 锁名
      * @param int $wait 加锁等待时间，单位：s
      * @param int $expire 锁有效时间，用于锁自动过期，防止死锁，单位：s
+     * @param string|int 锁持有者
      * @return boolean
      */
-    public function lock($name, $wait = 3, $expire = 10)
+    public function lock($name, $wait = 3, $expire = 10, $owner = null)
     {
         $res = false;
 
@@ -36,7 +37,7 @@ class RedisLockProvider extends Provider
             do {
                 $exTimestamp = time() + $expire;
                 $lockName = $this->realKey($name);
-                $res = $this->connection->set($lockName, $exTimestamp, array('nx', 'ex' => $expire));
+                $res = $this->connection->set($lockName, "$exTimestamp|$owner", array('nx', 'ex' => $expire));
                 $this->checkError();
                 // 提升效率,暂时不考虑未设置过期时间的问题
 /*                if (!($res = $this->driver->set($lockName, $exTimestamp, array('nx', 'ex' => $expire)))) {
@@ -71,15 +72,25 @@ class RedisLockProvider extends Provider
      * 解锁
      * 
      * @param string $name 锁名
+     * @param  string|int 锁的持有者，如果设置该参数则会检查锁是否属于该用户，属于才能解锁
      * @return boolean
      */
-    public function unlock($name)
+    public function unlock($name, $owner = null)
     {
         $res = false;
         if ($this->isLocking($name)) {
-            $res = $this->connection->del($this->realKey($name));
+            $realName = $this->realKey($name);
+            if ($owner) {
+                $lockInfo = explode('|', $this->connection->get($realName));
+                if (isset($lockInfo[1]) && $lockInfo[1] == $owner) {
+                    $this->connection->del($realName);
+                }
+            } else {
+                $res = $this->connection->del($realName);
+            }
             $this->checkError();
         }
-        return $res;
+        return (bool)$res;
     }
 }
+
